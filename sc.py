a6768947ebf99a5848c5a027791c644c34bbf413
mcrangler37 <144483135+mcrangler37@users.noreply.github.com>
2024-02-26 19:30:08 -0600

    Add files via upload

--- sc.py
+++ sc.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 #step 1
-startTime = time.time()
 driver = webdriver.Firefox()
 driver.get('https://twitter.com/mangorat4')
 time.sleep(.5)
@@ -22,26 +21,22 @@ soup = BeautifulSoup(resp, 'html.parser')
 ad = Details(soup.find("div",{"class":"r-1wvb978"}).text, soup.find("div",{"data-testid":"UserDescription"}).text)
 
 #step 4
-driver.get('https://twitter.com/i/flow/login')
-time.sleep(.5)
+time.sleep(1)
 pyautogui.press('tab', presses=4)
 
 #step 5
 pyautogui.write((ad.un[1:]))
 pyautogui.press('enter')
-time.sleep(.5)
+time.sleep(1)
 pyautogui.write(ad.pw[:ad.pw.find('"')])
 pyautogui.press('enter')
-time.sleep(.75)
+time.sleep(1)
 
 #step 6
-driver.get('https://twitter.com/mangorat4')
-time.sleep(.75)
+time.sleep(1)
 pyautogui.press('tab', presses=17)
 pyautogui.press('enter')
-time.sleep(.75)
-pyautogui.write('this tweet was entirely written by a python script i made to log in to my account and tweet this for me because im literally just that lazy')
+time.sleep(1)
+pyautogui.write('this tweet is brought to you by a shitty python algorithm')
 pyautogui.press('tab', presses=8)
-#pyautogui.press('enter')
-endTime = time.time()
-print('finished in ' + str((int(100 * (endTime - startTime))) / 100) + ' seconds')
+pyautogui.press('enter')
